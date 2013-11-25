4349c5398a10d0b75b09a9da54f7b708409b3b3a
Ilya Sher <ilya.sher@coding-knight.com>
2013-11-25 18:15:52 +0200

    work on subnet provisioner

---
+++ openstack_neutron_subnet_provisioner/tasks.py
@@ -0,0 +1,91 @@
+# vim: ts=4 sw=4 et
+
+# Generic
+import copy
+import json
+import os
+
+# Celery
+from celery import task
+
+# OpenStack
+import keystoneclient.v2_0.client as ksclient
+from neutronclient.neutron import client
+
+@task
+def provision(network_name, subnet, **kwargs):
+    neutron_client = _init_client()
+    n = _get_network_by_name_or_fail(neutron_client, network_name)
+    if _get_subnet_by_name(neutron_client, subnet['name']):
+        raise RuntimeError("Can not provision subnet with name '{0}' because subnet with such name already exists"
+                           .format(subnet['name']))
+
+    s = copy.deepcopy(subnet)
+    s['network_id'] = n['id']
+
+    print("XXX 1")
+    o = neutron_client.create_subnet({'subnet': s})
+    print("XXX 2")
+    print(o)
+
+@task
+def terminate(subnet, **kwargs):
+    neutron_client = _init_client()
+    sn = _get_subnet_by_name(neutron_client, subnet['name'])
+    neutron_client.delete_subnet(sn['id'])
+
+
+# TODO: cache the token, cache client
+def _init_client():
+    config_path = os.getenv('NEUTRON_CONFIG_PATH', os.path.expanduser('~/neutron_config.json'))
+    with open(config_path) as f:
+        neutron_config = json.loads(f.read())
+
+    keystone_client = _init_keystone_client()
+
+    neutron_client = client.Client('2.0', endpoint_url=neutron_config['url'], token=keystone_client.auth_token)
+    neutron_client.format = 'json'
+    return neutron_client
+
+
+def _init_keystone_client():
+    config_path = os.getenv('KEYSTONE_CONFIG_PATH', os.path.expanduser('~/keystone_config.json'))
+    with open(config_path) as f:
+        cfg = json.loads(f.read())
+    # Not the same config as nova client. Same parameters, different names.
+    args = {field: cfg[field] for field in ('username', 'password', 'tenant_name', 'auth_url')}
+    return ksclient.Client(**args)
+
+
+def _get_network_by_name(neutron_client, name):
+    # TODO: check whether neutron_client can get networks only named `name`
+    matching_networks = neutron_client.list_networks(name=name)['networks']
+
+    if len(matching_networks) == 0:
+        return None
+    if len(matching_networks) == 1:
+        return matching_networks[0]
+    raise RuntimeError("Lookup of network by name failed. There are {0} networks named '{1}'"
+                       .format(len(matching_networks), name))
+
+def _get_subnet_by_name(neutron_client, name):
+    # TODO: check whether neutron_client can get subnets only named `name`
+    matching_subnets = neutron_client.list_subnets(name=name)['subnets']
+
+    if len(matching_subnets) == 0:
+        return None
+    if len(matching_subnets) == 1:
+        return matching_subnets[0]
+    raise RuntimeError("Lookup of subnet by name failed. There are {0} subnets named '{1}'"
+                       .format(len(matching_subnets), name))
+
+def _get_network_by_name_or_fail(neutron_client, name):
+    network = _get_network_by_name(neutron_client, name)
+    if network:
+        return network
+    raise ValueError("Lookup of network by name failed. Could not find a network with name {0}".format(name))
+
+
+if __name__ == '__main__':
+    neutron_client = _init_client()
+    json.dumps(neutron_client.list_networks(), indent=4, sort_keys=True)
---
+++ openstack_neutron_subnet_provisioner/tests/test_openstack_neutron_subnet_provisioner.py
@@ -0,0 +1,60 @@
+# vim: ts=4 sw=4 et
+import argparse
+import json
+import logging
+import random
+import string
+import unittest
+
+import openstack_neutron_network_provisioner.tasks as network_tasks
+import openstack_neutron_subnet_provisioner.tasks as tasks
+
+RANDOM_LEN = 3  # cosmo_test_neutron_XXX_something
+
+class OpenstackSubnetProvisionerTestCase(unittest.TestCase):
+
+    def setUp(self):
+        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.logger = logging.getLogger("test_openstack_host_provisioner")
+        self.logger.level = logging.DEBUG
+        self.logger.info("setUp called")
+        self.neutron_client = tasks._init_client()
+        self.name_prefix = 'cosmo_test_neutron_{0}_'.format(''.join(
+            [random.choice(string.ascii_uppercase + string.digits) for i in range(RANDOM_LEN)]
+        ))
+        self.network_name = self.name_prefix + 'sub1'
+
+    def tearDown(self):
+        for net in self.neutron_client.list_networks()['networks']:
+            if net['name'].startswith(self.name_prefix):
+                self.neutron_client.delete_network(net['id'])
+
+    def test_all(self):
+
+        network = {'name': self.network_name}
+
+        network_tasks.provision(network)
+        network = network_tasks._get_network_by_name(self.neutron_client, self.network_name)
+        self.assertIsNotNone(network)
+        self.assertTrue(network['admin_state_up'])
+
+        name = self.name_prefix + 'sub1'
+        subnet = {
+            'name': name,
+            'cidr': '10.0.0.0/28',
+            'ip_version': 4,
+        }
+
+        sn = tasks.provision(network_name=self.network_name, subnet=subnet)
+        sn = tasks._get_subnet_by_name(self.neutron_client, name)
+        self.assertIsNotNone(sn)
+
+        tasks.terminate(subnet)
+        sn = tasks._get_subnet_by_name(self.neutron_client, name)
+        self.assertIsNone(sn)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+    unittest.main()
